ad9137cf492170219a7303cbc67151c97337f162
Alexander Eichhorn <alex@code-with.us>
2020-10-25 02:57:28 +0200

    http + rsync version

---
+++ Rsync/Launcher.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartGameButton = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.PictureBox();
+            this.downloadUpdates = new System.ComponentModel.BackgroundWorker();
+            this.versionsnr = new System.Windows.Forms.Label();
+            this.sm = new System.Windows.Forms.CheckBox();
+            this.status = new System.Windows.Forms.Label();
+            this.version_txt = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.StartGameButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartGameButton
+            // 
+            this.StartGameButton.BackColor = System.Drawing.Color.Transparent;
+            this.StartGameButton.Enabled = false;
+            this.StartGameButton.Image = global::ExtremLauncher.Properties.Resources.extrem_nonstart;
+            this.StartGameButton.InitialImage = global::ExtremLauncher.Properties.Resources.extrem_nonstart;
+            this.StartGameButton.Location = new System.Drawing.Point(161, 105);
+            this.StartGameButton.Name = "StartGameButton";
+            this.StartGameButton.Size = new System.Drawing.Size(340, 80);
+            this.StartGameButton.TabIndex = 7;
+            this.StartGameButton.TabStop = false;
+            this.StartGameButton.Click += new System.EventHandler(this.StartGamebtn_Click);
+            this.StartGameButton.MouseLeave += new System.EventHandler(this.StartGamebtn_MouseEnter);
+            this.StartGameButton.MouseHover += new System.EventHandler(this.StartGamebtn_MouseHover);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Transparent;
+            this.CloseButton.Image = global::ExtremLauncher.Properties.Resources.exit_off;
+            this.CloseButton.InitialImage = global::ExtremLauncher.Properties.Resources.exit_off;
+            this.CloseButton.Location = new System.Drawing.Point(12, 12);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(55, 42);
+            this.CloseButton.TabIndex = 8;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.Click += new System.EventHandler(this.Closebtn_Click);
+            this.CloseButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Closebtn_MouseDown);
+            this.CloseButton.MouseEnter += new System.EventHandler(this.Closebtn_MouseEnter);
+            this.CloseButton.MouseLeave += new System.EventHandler(this.Closebtn_MouseLeave);
+            // 
+            // versionsnr
+            // 
+            this.versionsnr.AutoSize = true;
+            this.versionsnr.BackColor = System.Drawing.Color.Transparent;
+            this.versionsnr.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versionsnr.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.versionsnr.Location = new System.Drawing.Point(863, 506);
+            this.versionsnr.Name = "versionsnr";
+            this.versionsnr.Size = new System.Drawing.Size(31, 13);
+            this.versionsnr.TabIndex = 15;
+            this.versionsnr.Text = "1.5T";
+            // 
+            // sm
+            // 
+            this.sm.AutoSize = true;
+            this.sm.BackColor = System.Drawing.Color.Transparent;
+            this.sm.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.sm.Location = new System.Drawing.Point(48, 491);
+            this.sm.Name = "sm";
+            this.sm.Size = new System.Drawing.Size(93, 18);
+            this.sm.TabIndex = 16;
+            this.sm.Text = "SoundMod";
+            this.sm.UseVisualStyleBackColor = false;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.BackColor = System.Drawing.Color.Transparent;
+            this.status.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(39)))), ((int)(((byte)(18)))));
+            this.status.Location = new System.Drawing.Point(71, 329);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 14);
+            this.status.TabIndex = 11;
+            // 
+            // version_txt
+            // 
+            this.version_txt.AutoSize = true;
+            this.version_txt.BackColor = System.Drawing.Color.Transparent;
+            this.version_txt.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.version_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.version_txt.Location = new System.Drawing.Point(270, 492);
+            this.version_txt.Name = "version_txt";
+            this.version_txt.Size = new System.Drawing.Size(56, 14);
+            this.version_txt.TabIndex = 18;
+            this.version_txt.Text = "Version:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(71, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(467, 42);
+            this.label1.TabIndex = 19;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ExtremLauncher.Properties.Resources.Launcherextrem_final;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(900, 523);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.version_txt);
+            this.Controls.Add(this.sm);
+            this.Controls.Add(this.versionsnr);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.StartGameButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DayZ Extrem Launcher";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.titelLeiste_MouseDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.titelLeiste_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.StartGameButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox StartGameButton;
+        private System.Windows.Forms.PictureBox CloseButton;
+        private System.ComponentModel.BackgroundWorker downloadUpdates;
+        private System.Windows.Forms.Label versionsnr;
+        private System.Windows.Forms.CheckBox sm;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label version_txt;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rsync/Launcher.cs
@@ -0,0 +1,286 @@
+﻿//------------------------------------------------------------------
+//-----------------------------Usings-------------------------------
+//------------------------------------------------------------------
+#region Usings
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+#endregion
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        //------------------------------------------------------------------
+        //---------------------Click Anywhere to Move-----------------------
+        //------------------------------------------------------------------
+        #region Click Anywhere to Move
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+        int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        #endregion
+
+        //------------------------------------------------------------------
+        //------------------Click Anywhere to Move Event--------------------
+        //------------------------------------------------------------------
+        #region Click Anywhere to Move Event
+        private void titelLeiste_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+        #endregion
+
+        //------------------------------------------------------------------
+        //-----------------------------Form1--------------------------------
+        //------------------------------------------------------------------
+        #region Form1
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!check_dir())
+            {
+                MessageBox.Show("Launcher liegt im falschen Verzeichnis", "Fehler!");
+                MessageBox.Show("Launcher muss ins OA Verzeichnis!", "Fehler hoch 2!");
+                MessageBox.Show("PACK MICH DA REIN!!!!", "Fehler hoch 3!");
+                Application.Exit();
+            }
+
+            version_txt.Text = "Version: " + get_online_version();
+            StartGameButton.Image = ExtremLauncher.Properties.Resources.extrem_rdy;
+            //set_text("Spiel ist auf dem neusten Stand");
+            StartGameButton.Enabled = true;
+        }
+        #endregion
+
+        public Boolean check_dir()
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\Expansion\\beta\\arma2oa.exe"))
+                return true;
+            else
+                return false;
+        }
+
+        public String get_online_version()
+        {
+            string myTextUrl = "http://dengpeng.eu/mod/e/version.php";
+            WebClient urlGrabber = new WebClient();
+            return urlGrabber.DownloadString(myTextUrl);
+        }
+
+        public String get_arma2ordner()
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\arma2_pfad.ini"))
+            {
+                using (StreamReader reader = new StreamReader(Directory.GetCurrentDirectory() + "\\arma2_pfad.ini", System.Text.Encoding.Default))
+                {
+                    return reader.ReadLine();
+                }
+            }
+            else
+            {
+                return Directory.GetCurrentDirectory();
+            }
+        }
+
+        public String get_extra_cmd()
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\e_extra_cmd.ini"))
+            {
+                using (StreamReader reader = new StreamReader(Directory.GetCurrentDirectory() + "\\e_extra_cmd.ini", System.Text.Encoding.Default))
+                {
+                    return reader.ReadLine();
+                }
+            }
+            else
+            {
+                return "-nosplash";
+            }
+        }
+
+        public String get_guid()
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\guid.ini"))
+            {
+                using (StreamReader reader = new StreamReader(Directory.GetCurrentDirectory() + "\\guid.ini", System.Text.Encoding.Default))
+                {
+                    return reader.ReadLine();
+                }
+            }
+            else
+            {
+                return "NO-GUID";
+            }
+        }
+
+        public Boolean erstelle_config()
+        {
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\ArmA 2\\ArmA2OA.cfg"))
+            {
+                using (StreamReader reader = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\ArmA 2\\ArmA2OA.cfg", System.Text.Encoding.Default))
+                {
+                    string line;
+                    string new_config = "";
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains("MinErrorToSend") || line.Contains("MaxMsgSend") || line.Contains("MaxSizeGuaranteed") || line.Contains("MaxSizeNonguaranteed") || line.Contains("MinBandwidth") || line.Contains("MaxBandwidth"))
+                        {
+                            using (var wb = new WebClient())
+                            {
+                                char trenn = '=';
+                                string[] split = line.Split(trenn);
+                                var data = new NameValueCollection();
+                                data.Add("guid", get_guid());
+                                data.Add("feld", split[0]);
+                                data.Add("wert", split[1]);
+                                data.Add("key", "dp");
+                                var response = wb.UploadValues("http://www.dengpeng.eu/report_config.php", "POST", data);
+                            }
+                        }
+                        else
+                        {
+                            new_config += line + "\r\n";
+                        }
+                    }
+                    reader.Close();
+                    if (File.Exists(Directory.GetCurrentDirectory() + "\\extrem.cfg"))
+                        File.Delete(Directory.GetCurrentDirectory() + "\\extrem.cfg");
+                    StreamWriter myFile = new StreamWriter("extrem.cfg");
+                    myFile.Write(new_config);
+                    myFile.Write("MinErrorToSend=0.001;\r\nMaxMsgSend=128;\r\nMaxSizeGuaranteed=1300;\r\nMaxSizeNonguaranteed=1300;\r\nMinBandwidth=256000;\r\nMaxBandwidth=480000;");
+                    myFile.Close();
+                    MessageBox.Show("Config geschrieben ...");
+                }
+            }
+            else
+                return false;
+            return false;
+        }
+
+        delegate void SetTextCallback(string text);
+
+        public void set_text(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.status.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(set_text);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.status.Text = text;
+            }
+        }
+
+        //------------------------------------------------------------------
+        //---------------------StartGameButton Events-----------------------
+        //------------------------------------------------------------------
+        #region StartGameButton Events
+        private void StartGamebtn_Click(object sender, EventArgs e)
+        {
+            if (check_dir())
+            {
+                Process Pu = new Process();
+                Pu.StartInfo.FileName = Directory.GetCurrentDirectory() + "\\rsync\\rsync.exe";
+                //Pu.StartInfo.UseShellExecute = true;
+                //Pu.StartInfo.RedirectStandardOutput = true;
+                string cygdrive = "\"/cygdrive/" + Directory.GetCurrentDirectory().Remove(1, 1).Replace("\\", "/") + "\"";
+                Pu.StartInfo.Arguments = " -htrv --del --progress moddownload.dengpeng.eu::extrem_test " + cygdrive + "/@DayZ_EX_TEST";
+                Pu.Start();
+                Pu.WaitForExit();
+                Pu.StartInfo.Arguments = " -htrv --del --progress moddownload.dengpeng.eu::cba_dp " + cygdrive + "/@CBA_DP";
+                Pu.Start();
+                Pu.WaitForExit();
+                Pu.StartInfo.Arguments = " -htrv --progress moddownload.dengpeng.eu::userconfig " + cygdrive + "/userconfig";
+                Pu.Start();
+                Pu.WaitForExit();
+                if (sm.Checked == true)
+                {
+                    Pu.StartInfo.Arguments = " -htrv --del --progress moddownload.dengpeng.eu::soundmod_dp " + cygdrive + "/@SoundMOD_DP";
+                    Pu.Start();
+                    Pu.WaitForExit();
+                }
+
+                string a2path = get_arma2ordner();
+                string extra_cmd = get_extra_cmd();
+
+                Process P = new Process();
+                P.StartInfo.FileName = Directory.GetCurrentDirectory() + "\\Expansion\\beta\\arma2oa.exe";
+                if (sm.Checked == true)
+                {
+                    if (erstelle_config())
+                        P.StartInfo.Arguments = "\"-mod=" + a2path + ";EXPANSION;ca\" -mod=Expansion\\beta;Expansion\\beta\\Expansion;@DayZ_EX_TEST;@CBA_DP;@SoundMOD_DP -cfg=extrem.cfg -connect=extremtest.dengpeng.eu -port=2401 " + extra_cmd; //  -nosplash -cpuCount=4 -exThreads=7
+                    else
+                        P.StartInfo.Arguments = "\"-mod=" + a2path + ";EXPANSION;ca\" -mod=Expansion\\beta;Expansion\\beta\\Expansion;@DayZ_EX_TEST;@CBA_DP;@SoundMOD_DP -connect=extremtest.dengpeng.eu -port=2401 " + extra_cmd; //  -nosplash -cpuCount=4 -exThreads=7
+                }
+                else
+                {
+                    if (erstelle_config())
+                        P.StartInfo.Arguments = "\"-mod=" + a2path + ";EXPANSION;ca\" -mod=Expansion\\beta;Expansion\\beta\\Expansion;@DayZ_EX_TEST;@CBA_DP -cfg=extrem.cfg -connect=extremtest.dengpeng.eu -port=2401 " + extra_cmd; //  -nosplash -cpuCount=4 -exThreads=7
+                    else
+                        P.StartInfo.Arguments = "\"-mod=" + a2path + ";EXPANSION;ca\" -mod=Expansion\\beta;Expansion\\beta\\Expansion;@DayZ_EX_TEST;@CBA_DP -connect=extremtest.dengpeng.eu -port=2401 " + extra_cmd; //  -nosplash -cpuCount=4 -exThreads=7
+                }
+                P.Start();
+            }
+            else
+            {
+                MessageBox.Show("Launcher liegt im falschen Verzeichnis", "Fehler!");
+                MessageBox.Show("Launcher muss ins OA Verzeichnis!", "Fehler hoch 2!");
+                MessageBox.Show("PACK MICH DA REIN!!!!", "Fehler hoch 3!");
+            }
+        }
+        private void StartGamebtn_MouseEnter(object sender, EventArgs e)
+        {
+            StartGameButton.Image = ExtremLauncher.Properties.Resources.extrem_rdy;
+        }
+        private void StartGamebtn_MouseHover(object sender, EventArgs e)
+        {
+            StartGameButton.Image = ExtremLauncher.Properties.Resources.extrem_start;
+        }
+        #endregion
+
+        //------------------------------------------------------------------
+        //----------------------CloseButton Events--------------------------
+        //------------------------------------------------------------------
+        #region CloseButton Events
+        private void Closebtn_MouseEnter(object sender, EventArgs e)
+        {
+            CloseButton.Image = ExtremLauncher.Properties.Resources.exit_over;
+        }
+        private void Closebtn_MouseLeave(object sender, EventArgs e)
+        {
+            CloseButton.Image = ExtremLauncher.Properties.Resources.exit_off;
+        }
+        private void Closebtn_MouseDown(object sender, MouseEventArgs e)
+        {
+            CloseButton.Image = ExtremLauncher.Properties.Resources.exit_over;
+        }
+        private void Closebtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+    }    
+}
